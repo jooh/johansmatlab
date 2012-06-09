a1dc7e2327cb32de39e86ef9ae20c1cff84c1f52
Johan Carlin <johan.carlin@gmail.com>
2012-06-09 17:35:35 +0100

    removed redundant code: distorigin==norm, npairs==nchoosek(x,2)

--- distorigin.m
+++
@@ -1,6 +0,0 @@
-% Compute euclidean distance from origin for entries in rows (dimensions in
-% columns). Trivial, but I keep mucking up the formula...
-% d = distorigin(data)
-function d = distorigin(data)
-
-d = sqrt(sum(data.^2,2));
--- npairs.m
+++
@@ -1,4 +0,0 @@
-% Return number of unique pairs for a given scalar n.
-% np = npairs(n)
-function np = npairs(n)
-np = ((n.^2)-n)/2;
