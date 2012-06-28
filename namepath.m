4b36b0ae5798c0be2cf919599d7dd40b8c610f86
Johan Carlin <johan.carlin@gmail.com>
2012-06-28 15:06:31 +0100

    new function to return fun name and directory

---
+++ namepath.m
@@ -0,0 +1,13 @@
+% Return function's name and directory (or optionally the name/path of the
+% function n steps up the stack)
+% [fname,fpath] = namepath(n)
+function [fname,fpath] = namepath(n)
+
+if ieNotDefined('n')
+    n = 1;
+end
+n = n+1;
+st = dbstack;
+assert(length(st)<=n,'n exceeds stack length');
+fname = st(n).name;
+fpath = fileparts(which(fname));
