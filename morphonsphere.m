faa6a41e1a1333fdc0b7ae75e8eef2aa940562ff
Johan Carlin <johan.carlin@gmail.com>
2012-07-31 19:35:02 +0100

    morphonsphere now uses optimisation toolbox, should be more robust

--- morphonsphere.m
+++ morphonsphere.m
@@ -3,21 +3,11 @@
 % assumed to be on. s==1 gives all a, s==0 gives all b. 
 %
 % This should be solvable analytically but in the absence of such a
-% solution we use some ugly while loops to approximate the desired ratio of
-% euclidean distances, (ie pdist([a c]) / pdist([b c])). You can customise
-% the search precision by setting tolerance (default 1e-2) and number of
-% iterations to try before crashing (default 10e3). 
+% solution we use a fminbnd search to find the appropriate morph level to
+% achieve the desired ab angle, ie angvec(a,b)*s.
 %
-% c = morphonsphere(a,b,s,[tolerance],[totiter]);
-function c = morphonsphere(a,b,s,tolerance,totiter)
-
-if ieNotDefined('tolerance')
-    tolerance = 1e-2;
-end
-
-if ieNotDefined('totiter')
-    totiter = 10e3;
-end
+% c = morphonsphere(a,b,s)
+function c = morphonsphere(a,b,s)
 
 assert(ndims(a)==ndims(b),'coordinates must have same dimensionality')
 assert(all(size(a)==size(b)),'coordinates a and b must be the same size')
@@ -35,36 +25,37 @@ assert(isscalar(s),'scale factor s must be scalar')
 % sadly no support for caricaturing/anticaricaturing at present
 assert(inrange(s,[0 1])==s,'scale factor s must be in 0:1 range')
 
-% start out at the right point in euclidean space to speed things up
-c = seteccentricity(morph(a,b,s),normdist);
-
 % shortcircuit processing for speed when in simple cases where the
 % euclidean distance is the same as the surface distance
+c = seteccentricity(morph(a,b,s),normdist);
 if any(s == [0 1 .5])
     return
 end
+halfdist = seteccentricity(morph(a,b,.5),normdist);
 
-% The tricky thing here is that the euclidean distance is necessarily LESS
-% than the surface distance so if you only optimise ac or bc you will end
-% up stopping short of the correct point. Instead, optimise the ratio of
-% bc/ba
-targrate = s / (1-s);
-
-niter = 0;
-m = 1;
-corg = c;
-distratefun = @(a,b,c) pdist([c b]') / pdist([c a]');
-distrate = distratefun(a,b,c);
-distdiff = distrate - targrate;
-while abs(distdiff) > tolerance
-    niter = niter+1;
-    if distdiff<0
-        m = m * 1.01;
-    else
-        m = m * 0.99;
-    end
-    c = seteccentricity(morph(corg,b,m),normdist);
-    distrate = distratefun(a,b,c);
-    distdiff = distrate - targrate;
-    assert(niter < totiter,'failed to converge')
+% Rather than morphing between a and b, morph between ac or bc. This is
+% useful for cases where b is near-opposite a
+if s < .5
+    % somewhere between b and halfdist
+    org = b;
+    % the angle we originally wanted was between b (0) and a (1). Now it's
+    % between b (0) and halfdist (.5), so need to scale things up.
+    ang_target = angvec(org,halfdist) * s*2;
+else
+    % somewhere between a and halfdist
+    org = a;
+    % the angle we originally wanted was between b (0) and a (1). Now it's
+    % between a (1) and halfdist (.5), so need to change sign and scale
+    ang_target = angvec(org,halfdist) * (1-s)*2;
 end
+
+% optimise the absolute difference between the org-c angle and the target
+% angle
+ofun = @(m) abs(angvec(org,morph(org,halfdist,m))-ang_target);
+% minute changes in morph level produce huge angle changes so need to
+% iterate to fairly high precision
+opts = optimset('TolX',1e-100,'TolFun',1e-100,'MaxIter',10e3);
+[m,angdiff,exflag,output] = fminbnd(ofun,0,1,opts);
+assert(exflag==1,'optimisation did not converge')
+% produce the output vector with the same length as the hypersphere radius
+c = seteccentricity(morph(org,halfdist,m),normdist);
