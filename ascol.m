2271b735919820a5b23afcbc9bbae2efd56e917e
Johan Carlin <johan.carlin@gmail.com>
2012-08-06 18:51:50 +0100

    added n return input to ascol and asrow for convenient collection of subset of fun returns

--- ascol.m
+++ ascol.m
@@ -1,4 +1,7 @@
-% The dumbest matlab function ever
-% x = ascol(x)
-function x = ascol(x)
+% Return as a column, optionally only first n elements.
+% x = ascol(x,[n])
+function x = ascol(x,n)
 x = x(:);
+if ~ieNotDefined('n')
+    x = x(1:n);
+end
--- asrow.m
+++ asrow.m
@@ -1,4 +1,7 @@
-% The dumbest matlab function ever (2)
-% x = asrow(x)
-function x = asrow(x)
+% Return as a row, optionally only first n elements.
+% x = asrow(x,[n])
+function x = asrow(x,n)
 x = x(:)';
+if ~ieNotDefined('n')
+    x = x(1:n);
+end
