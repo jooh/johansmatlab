06b4d6a3a0351ec1d597097af74541b182bc69ec
Johan Carlin <johan.carlin@gmail.com>
2012-12-12 16:57:43 +0000

    convenience function for getting absolute mean differences out of pdist

---
+++ meandist.m
@@ -0,0 +1,11 @@
+% a (abs) mean distance function to be used with pdist(x,@meandist). Mainly
+% useful for getting differences between scalars.
+% % D = angdist(a,b)
+function  D = meandist(XI,XJ)
+
+nj = size(XJ,1);
+
+D = NaN([1 nj]);
+for n = 1:nj
+    D(n) = abs(mean(XI,2) - mean(XJ(n,:),2));
+end
