a3883232b1d0903ffe84658ac3f11b7953eff9db
Johan Carlin <johan.carlin@gmail.com>
2013-02-26 15:26:01 +0000

    fixed bad dprime behaviour with small n

--- dprime.m
+++ dprime.m
@@ -2,16 +2,26 @@
 % d = dprime(hits,misses,falsealarms,corrrejs)
 function d = dprime(hits,misses,falsealarms,corrrejs)
 
+modifier = .0001;
+hits(hits==0) = modifier;
+misses(misses==0) = modifier;
+falsealarms(falsealarms==0) = modifier;
+corrrejs(corrrejs==0) = modifier;
 pHits = hits ./ (hits + misses) ;
 pHits(isnan(pHits)) = 0;
 pFAs = falsealarms ./ (falsealarms + corrrejs);
 pFAs(isnan(pFAs)) = 0;
-modifier = .01;
 
 % Avoid infinity (effectively caps d at 4.65)
-zHits = norminv(pHits);
-zHits(isinf(zHits)) = norminv(1-modifier);
-zFAs = norminv(pFAs);
-zFAs(isinf(zFAs)) = norminv(modifier);
+zHits = norminvinfcorrect(pHits);
+zFAs = norminvinfcorrect(pFAs);
 
 d = zHits - zFAs;
+
+function z = norminvinfcorrect(ps)
+modifier = .001;
+z = norminv(ps);
+infpos = isinf(z) & z>0;
+infneg = isinf(z) & z<0;
+z(infpos) = norminv(ps(infpos)-modifier);
+z(infneg) = norminv(ps(infneg)+modifier);
