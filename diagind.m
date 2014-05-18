c028f92046a636142182c984df8b67451ba68542
Johan Carlin <johan.carlin@gmail.com>
2014-05-18 18:57:04 +0100

    better error checking

--- diagind.m
+++ diagind.m
@@ -1,7 +1,15 @@
+% return diagonal indices for a 2D or 3D matrix of size sz (must be square
+% in first and second dimensions).
+%
+% ind = diagind(sz)
 function ind = diagind(sz)
 
 assert(sz(1)==sz(2),'matrix must be square in first two dims');
-if numel(sz)==2
+
+ndim = numel(sz);
+assert(ndim<4,'matrix must be 3D at most');
+
+if ndim==2
     sz(3) = 1;
 end
 
