f2291b9118bdba6a541148928ee8d86c2966b7be
Johan Carlin <johan.carlin@gmail.com>
2014-07-30 17:36:47 +0900

    now supports nd matrices

--- diagind.m
+++ diagind.m
@@ -7,10 +7,10 @@ function ind = diagind(sz)
 assert(sz(1)==sz(2),'matrix must be square in first two dims');
 
 ndim = numel(sz);
-assert(ndim<4,'matrix must be 3D at most');
+%assert(ndim<4,'matrix must be 3D at most');
 
 if ndim==2
     sz(3) = 1;
 end
 
-ind = repmat(logical(eye(sz(1))),[1 1 sz(3)]);
+ind = repmat(logical(eye(sz(1))),[1 1 sz(3:end)]);
