09aa3100d99e045944baca1003f34c0d78d91f87
Johan Carlin <johan.carlin@gmail.com>
2014-10-22 11:48:03 +0100

    corrpairs fixed and moved to pilab/util

--- corrpairs.m
+++
@@ -1,7 +0,0 @@
-% return a vector of the paired correlations between the columns in a and
-% b (over rows).
-%
-% r = corrpairs(a,b)
-function r = corrpairs(a,b)
-
-r = dot(unitlength(zeromean(a,1),1),unitlength(zeromean(a,1),1));
