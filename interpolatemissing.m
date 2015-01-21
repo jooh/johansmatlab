2adb792d110b4cd45d8d343ee29b37cf5a3de2b8
Johan Carlin <johan.carlin@gmail.com>
2015-01-21 18:16:21 +0000

    refactored for robustness with eye tracking data

--- interpolatemissing.m
+++ interpolatemissing.m
@@ -1,36 +1,41 @@
 % Perform linear interpolation on missing values (value==target, default
-% NaN) in 1D vector d.
+% NaN) in vector d operating along the columns of d
 %
 % Missing values at the beginning and end of the time series get
 % replaced with a constant (first/last valid value).
 %
 % dclean = interpolatemissing(d,target)
-function d = interpolatemissing(d,target)
+function alld = interpolatemissing(alld,target)
 
 if ieNotDefined('target')
   target = NaN;
 end
 
 if isnan(target)
-  missing = isnan(d);
+  missing = isnan(alld);
 else
-  missing = d == target;
+  missing = alld == target;
 end
 valid = ~missing;
 
-% check for missing start/end, replace with constant
-if missing(1)
-  firstvalid = find(valid,1,'first');
-  d(1:firstvalid) = d(firstvalid);
-  valid(1:firstvalid) = true;
-  missing(1:firstvalid) = false;
-end
-if missing(end)
-  lastvalid = find(valid,1,'last');
-  d(lastvalid:end) = d(lastvalid);
-  valid(lastvalid:end) = true;
-  missing(lastvalid:end) = false;
-end
+for c = 1:size(alld,2)
+    d = alld(:,c);
+    % check for missing start/end, replace with constant
+    if missing(1,c)
+      firstvalid = find(valid(:,c),1,'first');
+      d(1:firstvalid) = d(firstvalid);
+      valid(1:firstvalid) = true;
+      missing(1:firstvalid,c) = false;
+    end
+    if missing(end,c)
+      lastvalid = find(valid(:,c),1,'last');
+      d(lastvalid:end) = d(lastvalid);
+      valid(lastvalid:end,c) = true;
+      missing(lastvalid:end,c) = false;
+    end
 
-% do linear interpolation on the rest
-d(missing) = interp1(find(valid),d(valid),find(missing),'linear');
+    % do linear interpolation on the rest
+    d(missing(:,c)) = interp1(find(valid(:,c)),d(valid(:,c)),...
+        find(missing(:,c)),'linear');
+    alld(:,c) = d;
+end
