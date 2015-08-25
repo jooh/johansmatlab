a418ce8c89380776b0b29759625f07da0834a360
Johan Carlin <johan.carlin@gmail.com>
2015-08-25 10:55:23 +0100

    s on original scale rather than powers of 10

--- sigmoidlike.m
+++ sigmoidlike.m
@@ -1,11 +1,11 @@
 % return a sigmoid-like function of the form
-% y = 1 ./ (1+exp((-x+offset)./10^-s));
+% y = 1 ./ (1+exp((-x+offset)./s));
 %
-% s controls the steepness of the saturation (smaller for more linear,
-% larger for more step-like), and is scored in negative powers of 10. 
-% offset is the horizontal offset of the function (its steepest portion).
+% s controls the steepness of the saturation (smaller for more step-like,
+% larger for more linear). Note that negative values flips the direction of
+% the function.
 %
 % y = sigmoidlike(x,s,offset)
 function y = sigmoidlike(x,s,offset)
 
-y = 1 ./ (1+exp((-x+offset)./10^-s));
+y = 1 ./ (1+exp((-x+offset)./s));
