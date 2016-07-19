6c91fb8f72363f00783c126840a433450be5b012
Johan Carlin <johan.carlin@gmail.com>
2016-07-19 17:19:24 +0100

    support array expansion to enable morphing with regard to scalars

--- morph.m
+++ morph.m
@@ -5,11 +5,13 @@
 function c = morph(a,b,s);
 
 assert(isscalar(s),'scale factor s must be scalar')
-assert(ndims(a)==ndims(b),'coordinates must have same dimensionality')
-assert(all(size(a)==size(b)),'coordinates a and b must be the same size')
 
 % to intuit what happens here, b is made the norm of the space by
 % subtraction. The normed a is then multiplied by a scalar to move
 % away/towards normed b. Finally, b is added to translate the space back to
 % it original norm.
-c = (a-b)*s+b;
+% c = (a-b)*s+b;
+% below we add array expansion - so as long as the inputs share one
+% dimension length we can morph. This is useful for e.g. translating each
+% row or column toward its average, or shrinking toward zero.
+c = bsxfun(@plus,bsxfun(@minus,a,b)*s,b);
